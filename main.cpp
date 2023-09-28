f7fc1e28b665de1a6cc600bc0ddc1a93f9b633a0
ruslanvgu <ruslanvgu@mail.ru>
2023-09-28 23:04:40 +0300

    change

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include <json/json.h>
 #include <grpcpp/grpcpp.h>
 
 #include "my_service.grpc.pb.h"
 
-using grpc::Server;
+/*using grpc::Server;
 using grpc::ServerBuilder;
 using grpc::ServerContext;
 using grpc::Status;
@@ -20,21 +21,52 @@ class MyServiceImpl final : public MyService::Service {
   }
 };
 
-void RunServer() {
-  std::string server_address("0.0.0.0:50051");
-  MyServiceImpl service;
+}*/
+void readConfig(){
+  std::ifstream jsonFile("config.json");
+  Json::Value root;
+  Json::Reader reader;
+  bool parsingSuccessful = reader.parse(jsonFile, root);
+  if (parsingSuccessful) {
+    std::string serverAddress = root["grpc"]["server_address"].asString();
+    int port = root["grpc"]["port"].asInt();
+    
+  } else {
+    // Ошибка парсинга JSON
+  }
+}
 
-  ServerBuilder builder;
+
+class KeepAliveServiceImpl final : public KeepAliveService::Service {
+public:
+  grpc::Status KeepAlive(grpc::ServerContext* context, const Empty* request, Empty* response) override {
+    std::cout << "Method called: keep_alive" << std::endl;
+    //  логика здесь, например:
+    // doSomething();
+    return grpc::Status::OK;
+  }
+};
+
+
+int main(int argc, char** argv) {
+
+  
+
+
+  std::string server_address("0.0.0.0:50051");
+
+  KeepAliveServiceImpl service;
+  grpc::ServerBuilder builder;
   builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
   builder.RegisterService(&service);
 
-  std::unique_ptr<Server> server(builder.BuildAndStart());
+  std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
   std::cout << "Server listening on " << server_address << std::endl;
-
   server->Wait();
+
+  return 0;
 }
 
-int main(int argc, char** argv)
-{
-    
-}
+//protoc -I <путь_к_файлу_proto> --grpc_out=<путь_для_генерации_кода> --plugin=protoc-gen-grpc=<путь_к_grpc_cpp_plugin> <путь_к_файлу_proto>
+//
+//g++ -std=c++11 service.pb.cc service.grpc.pb.cc main.cpp -o server `pkg-config --cflags --libs grpc++ grpc`
